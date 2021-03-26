bde35a094e0df03330a5997595a8731042ccd05e
Patrick Lee <patricklee1138@live.com>
2021-03-26 10:46:40 -0700

    BUGFIX: correct branch logic exception

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
 // BUSINESS LOGIC
 function results(inp1, inp2, inp3, inp4, inp5, inp6) {
-  // STUFF
   alert(`Here's what to say to ${inp1}: "${inp2} My name is ${inp3} My pronouns are ${inp4}. ${inp5} ${inp6}"`)
 
+  // Extrapolate some data from inputs:
+  // Group inputs into pairs, add their string lengths together, and assign value "0" for evens and "1" for odds
+  const count1 = (inp1.length + inp2.length) % 2
+  const count2 = (inp3.length + inp4.length) % 2
+  const count3 = (inp5.length + inp6.length) % 2
+  // Tally the results, which must range from "0" (all evens) to "3" (all odds)
+  const totalCount = count1 + count2 + count3
+  alert(`The totalCount variable has the value ${totalCount}.`)
+
   // Use branching to return at least 3 different results
-  if (inp1.length <= 5) {
-    alert(`short input`)
-  } else if (inp1.length > 5) {
-    alert(`long input`)
+  if (totalCount === 0) {
+    alert(`result 1`)
+  } else if (totalCount === 1) {
+    alert(`result 2`)
+  } else if (totalCount === 2) {
+    alert(`result 3`)
+  } else if (totalCount === 3) {
+    alert(`result 4`)
   } else {
-    alert(`input exception`)
+    alert(`branch logic exception`)
   }
 }
 
