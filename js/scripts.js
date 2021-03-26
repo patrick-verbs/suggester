a55523d39d839370651623bad3ddb6957f6eb9a5
Patrick Lee <patricklee1138@live.com>
2021-03-26 08:57:48 -0700

    add basic form handling in HTML & JS

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// BUSINESS LOGIC
+function results() {
+  // STUFF
+  alert(`Function has received the inputs ${inp1}, ${inp2}, ${inp3}.`)
+}
+
+
+// USER INTERFACE LOGIC
+$(document).ready(function() {
+  console.log("Document ready for user input.")
+  // Get form data:
+  $("form#form1").submit(function() {
+    // Prevent the form data from being pushed to a server,
+    // since all data is being handled here in JS/CSS/HTML:
+    event.preventDefault()
+
+    // Set variables to form inputs for readability:
+    const answer1 = $("input#quest1").text()
+    const answer2 = $("input#quest1").text()
+    const answer3 = $("input#quest1").text()
+
+    // Pass form inputs (as variables) to a business-logic
+    // function as parameters:
+    results(answer1, answer2, answer3)
+  })
+})
