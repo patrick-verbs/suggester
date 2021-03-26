cfcdc5e61b11f6d375859d52409be5a4e429f6f0
Patrick Lee <patricklee1138@live.com>
2021-03-26 11:55:29 -0700

    implement array 'counts' to simplify error handling & scaling

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ function results(inp1, inp2, inp3, inp4, inp5, inp6) {
   alert(`Here's what to say to ${inp1}: "${inp2} My name is ${inp3} My pronouns are ${inp4}. ${inp5} ${inp6}"`)
 
   // Extrapolate some data from inputs:
-  // Group inputs into pairs, add their string lengths together, and assign value "0" for evens and "1" for odds
-  const count1 = (inp1.length + inp2.length) % 2
+  // Group inputs into pairs, add their string-lengths together, and assign value "0" for evens and "1" for odds
+  const count1 = (inp1.length + inp2.length) % 2// Always returns "0" or "1"
   const count2 = (inp3.length + inp4.length) % 2
   const count3 = (inp5.length + inp6.length) % 2
-  // Tally the results, which must range from "0" (all evens) to "3" (all odds)
-  let totalCount = count1 + count2 + count3
+  // Tuck constants into an array for simplified error prevention and legible future scaling:
+  const counts = [count1, count2, count3]
+  // Tally the results using the array to prevent unpredictable results:
+  const totalCount = counts[0] + counts[1] + counts[2]
   // Catch errors in the above input handling and variable assignment:
-  totalCount = totalCount.toString()
-  alert(`totalCount: ${totalCount}`)
-  if ( totalCount.isNaN() === true || totalCount < 0 || totalCount > 3 || totalCount != parseInt(totalCount) ) {
+  if ( totalCount < 0 || totalCount > counts.length || totalCount != parseInt(totalCount) ) {
     alert(`Something went wrong passing user input data to the totalCount variable. It currently has the value of "${totalCount}".`)
   } else {
     alert(`So far, so good!`)
@@ -20,15 +20,18 @@ function results(inp1, inp2, inp3, inp4, inp5, inp6) {
 
   // Use branching to return at least 3 different results
   if (totalCount === 0) {
-    alert(`result 1`)
+    alert(`Result 1:`
+    + `\r\nTotalCount = ${totalCount}`)
   } else if (totalCount === 1) {
-    alert(`result 2`)
+    alert(`Result 2: totalCount = ${totalCount}`)
   } else if (totalCount === 2) {
-    alert(`result 3`)
+    alert(`Result 3: totalCount = ${totalCount}`)
   } else if (totalCount === 3) {
-    alert(`result 4`)
+    alert(`Result 4: totalCount = ${totalCount}`)
   } else {
-    alert(`Branch logic exception. This logic uses the totalCount variable.`)
+    alert(`Branch logic exception.`
+    + `\r\nThis branching uses the totalCount variable, which should have a value from 0 to ${counts.length}.`
+    + `\r\nIt currently has a value of ${totalCount}.`)
   }
 }
 
