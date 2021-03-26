8b000d694e5881bca7cbab1162ba5b58bc051011
Patrick Lee <patricklee1138@live.com>
2021-03-26 10:24:19 -0700

    add JS branching based on inputs

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@
 function results(inp1, inp2, inp3, inp4, inp5, inp6) {
   // STUFF
   alert(`Here's what to say to ${inp1}: "${inp2} My name is ${inp3} My pronouns are ${inp4}. ${inp5} ${inp6}"`)
+
+  // Use branching to return at least 3 different results
+  if (inp1.length <= 5) {
+    alert(`short input`)
+  } else if (inp1.length > 5) {
+    alert(`long input`)
+  } else {
+    alert(`input exception`)
+  }
 }
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
-  console.log("Document ready for user input.")
-  // Get form data:
+  // Receive the form data:
   $("#form1").submit(function() {
     // Prevent the form data from being pushed to a server,
     // since all data is being handled here in JS/CSS/HTML:
